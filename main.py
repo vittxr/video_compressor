8a3eaff80348b622a4c0a69b0548bee6b73f9545
Vitor <vitor.roberto3022@gmail.com>
2023-05-12 11:57:42 -0300

    compress video sended as form

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import io
 import os
 import subprocess
 import tempfile
-from fastapi import Depends, FastAPI, Request, UploadFile 
-from pydantic import BaseModel
+from fastapi.responses import StreamingResponse
+from fastapi import FastAPI, Form, UploadFile 
 
 app = FastAPI()
 
-def compress_video(video_bytes, ext: str = 'mp4'):
+async def compress_video(video, filename: str, ext: str):
     # Read the video file from the FileStorage object
     # Write the video bytes to a temporary file
+    video_bytes = await video.read()
     with tempfile.NamedTemporaryFile(delete=False) as f:
         f.write(video_bytes)
         input_path = f.name
@@ -16,12 +18,10 @@ def compress_video(video_bytes, ext: str = 'mp4'):
     # Compress the video using ffmpeg
     output_path = f'compressed_video.{ext}'
     command = ['ffmpeg',
-               '-threads', '3',
                '-i', input_path,
                '-c:v', 'libx264',
                '-preset', 'medium',
-               '-crf', '28',
-               '-f', 'mp4', 
+               '-crf', '45',
                output_path]
     subprocess.run(command, check=True)
 
@@ -33,16 +33,16 @@ def compress_video(video_bytes, ext: str = 'mp4'):
     os.remove(input_path)
     os.remove(output_path)
 
-    # Return the compressed video as a bytes object
-    return compressed_video 
+    return compressed_video
 
 @app.post('/compress_form_data_video/')
-async def compress_form_data_video_route(file: UploadFile):
-    video_bytes = await file.read()
-    video_bytes = compress_video(video_bytes, file.filename.split('.')[-1].lower)
-    return video_bytes
+async def compress_form_data_video_route(file: UploadFile, filename: str = Form(None), ext: str = Form(None)):  
+    video_bytes = await compress_video(file, 
+                                       filename or file.filename,
+                                       ext or file.filename.split('.')[-1].lower())
+    # Return a response with the video bytes
+    return StreamingResponse(io.BytesIO(video_bytes), media_type='application/octet-stream')
 
 if __name__ == '__main__':
-    # uvicorn.run(app, host='127.0.0.1', port=8000, reload=True)
     import uvicorn 
     uvicorn.run('main:app', host='0.0.0.0', port=8000, log_level='info', reload=True)
